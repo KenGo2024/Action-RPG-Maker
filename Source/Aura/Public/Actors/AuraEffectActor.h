84f65ba6653dfaaa06d4a3931a0526d5058a7b92
unknown <kts.dohoanglinh@gmail.com>
2024-07-23 10:29:52 +0700

    Widget and WidgetController

--- Source/Aura/Public/Actors/AuraEffectActor.h
+++ Source/Aura/Public/Actors/AuraEffectActor.h
@@ -16,7 +16,6 @@ class AURA_API AAuraEffectActor : public AActor
 public:	
 
 	AAuraEffectActor();
-	git
 	UFUNCTION()
 	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
